4ae824a2afbc2ad8e135ce55d6627c18155eafe6
mrzhaojunjie <1069144207@qq.com>
2019-12-07 16:11:05 +0800

    删除了个别项目

--- Object/图书馆管理系统/Book.java
+++
@@ -1,8 +0,0 @@
-package 图书馆管理系统;
-
-public class Book {
-    String title; //书名
-    String author;//作者
-    int currentCount;//当前数量
-    int totalCount;//总量
-}
--- Object/图书馆管理系统/User.java
+++
@@ -1,7 +0,0 @@
-package 图书馆管理系统;
-
-public abstract class User {
-     public abstract void menu();
-     public abstract void operation(int select);
-
-}
